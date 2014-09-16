53c586c7b783759c1ebf05131a6b9647192b9c9d
Melnikov Sergey <me@rnr.name>
2014-09-16 11:48:29 +0700

    Added configuring modules.

--- definitions/php_oc_extention.rb
+++ definitions/php_oc_extention.rb
@@ -2,10 +2,25 @@ define :php_oc_extention, :module => nil, :action => nil, :config => nil, :type 
   unless %w{install uninstall}.include?(params[:action])
     raise 'Set required params'
   end
+
+  if params[:action].is_a?(Hash)
+    data = params[:action]
+    action = params[:action]['action']
+  else
+    action = params[:action]
+  end
+
   params[:module] ||= params[:name]
-  execute "php cli/index.php extensions/#{params[:type]} '#{params[:action]}' '#{params[:module]}'" do
-    command "php cli/index.php extensions/#{params[:type]} '#{params[:action]}' '#{params[:module]}'"
+  execute "php cli/index.php extensions/#{params[:type]} '#{action}' '#{params[:module]}'" do
     cwd node['deploy-project']['path']
-    action :run
+  end
+
+  unless data.nil?
+    require 'json'
+
+    json_data = configure_data.to_json
+    execute "echo '#{json_data}' | php cli/index.php extensions/#{params[:type]} 'configure' '#{params[:module]}'" do
+      cwd node['deploy-project']['path']
+    end
   end
 end
